c7cd6ff9b89c2d5e9f49d6acab352cc53ac42904
Ward Pieters <ward@wardpieters.nl>
2022-10-05 09:17:26 +0200

    fix binary toString functions

--- src/datastructures/BinaryTree/BinaryTree.cs
+++ src/datastructures/BinaryTree/BinaryTree.cs
@@ -70,7 +70,7 @@ namespace AD
 
         public bool IsEmpty()
         {
-            return Size() == 0;
+            return root == null;
         }
 
         public void Merge(T rootItem, BinaryTree<T> t1, BinaryTree<T> t2)
@@ -80,32 +80,47 @@ namespace AD
         
         public string ToPrefixString()
         {
-            if (root == null)
+            return ToPrefixString(root);
+        }
+        
+        private string ToPrefixString(BinaryNode<T> node)
+        {
+            if (node == null)
             {
                 return "NIL";
             }
-            
-            throw new System.NotImplementedException();
+
+            return $"[ {node.data.ToString()} {ToPrefixString(node.left)} {ToPrefixString(node.right)} ]";
         }
 
         public string ToInfixString()
         {
-            if (root == null)
+            return ToInfixString(root);
+        }
+
+        public string ToInfixString(BinaryNode<T> node)
+        {
+            if (node == null)
             {
                 return "NIL";
             }
             
-            throw new System.NotImplementedException();
+            return $"[ {ToInfixString(node.left)} {node.data.ToString()} {ToInfixString(node.right)} ]";
         }
 
         public string ToPostfixString()
         {
-            if (root == null)
+            return ToPostfixString(root);
+        }
+
+        private string ToPostfixString(BinaryNode<T> node)
+        {
+            if (node == null)
             {
                 return "NIL";
             }
             
-            throw new System.NotImplementedException();
+            return $"[ {ToPostfixString(node.left)} {ToPostfixString(node.right)} {node.data.ToString()} ]";
         }
 
 
