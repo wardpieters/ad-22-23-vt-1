506641846c6aac4048d39b764136f3c61c7a8976
Ward Pieters <ward@wardpieters.nl>
2022-10-05 10:26:10 +0200

    wip remove bst

--- src/datastructures/BinarySearchTree/BinarySearchTree.cs
+++ src/datastructures/BinarySearchTree/BinarySearchTree.cs
@@ -47,22 +47,110 @@ namespace AD
 
         public T FindMin()
         {
-            throw new System.NotImplementedException();
+            return FindMin(root);
+        }
+
+        private T FindMin(BinaryNode<T> node)
+        {
+            if (node == null)
+            {
+                throw new BinarySearchTreeEmptyException();
+            }
+            
+            if (node.left == null)
+            {
+                return node.data;
+            }
+            
+            return FindMin(node.left);
         }
 
         public void RemoveMin()
         {
-            throw new System.NotImplementedException();
+            if (root == null)
+            {
+                throw new BinarySearchTreeEmptyException();
+            }
+
+            if (root.left == null)
+            {
+                root = root.right;
+            }
+            else
+            {
+                RemoveMin(root);
+            }
+        }
+        
+        private void RemoveMin(BinaryNode<T> node)
+        {
+            if (node.left.left == null)
+            {
+                node.left = node.left.right;
+            }
+            else
+            {
+                RemoveMin(node.left);
+            }
         }
 
         public void Remove(T x)
         {
-            throw new System.NotImplementedException();
+             Remove(x, root);
+        }
+
+        private void Remove(T x, BinaryNode<T> node)
+        {
+            if (node == null)
+            {
+                throw new BinarySearchTreeElementNotFoundException();
+            }
+
+            if (x.Equals(node.data))
+            {
+                if (node.right == null)
+                {
+                    node.right = node.left.right;
+                    node.data = node.left.data;
+                    node.left = node.left.left;
+                }
+                
+                T min = FindMin(node.right);
+                RemoveMin(node);
+
+                node.data = min;
+            }
+            else
+            {
+                if (x.CompareTo(node.data) > 0)
+                {
+                    Remove(x, node.right);
+                }
+                else if (x.CompareTo(node.data) < 0)
+                {
+                    Remove(x, node.left);
+                }
+            }
         }
 
         public string InOrder()
         {
-            throw new System.NotImplementedException();
+            return InOrder(root);
+        }
+
+        private string InOrder(BinaryNode<T> node)
+        {
+            if (node == null)
+            {
+                return "";
+            }
+
+            return $"{InOrder(node.left)} {node.data} {InOrder(node.right)}".Trim();
+        }
+
+        public override string ToString()
+        {
+            return InOrder();
         }
     }
 }
