25f9ca6f93dba68d341cb280e1ee72edd3a8fbb9
Ward Pieters <ward@wardpieters.nl>
2022-10-07 13:39:25 +0200

    wip priority queue

--- src/datastructures/PriorityQueue/PriorityQueue.cs
+++ src/datastructures/PriorityQueue/PriorityQueue.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 
-
 namespace AD
 {
-    public partial class PriorityQueue<T> : IPriorityQueue<T>
-        where T : IComparable<T>
+    public partial class PriorityQueue<T> : IPriorityQueue<T> where T : IComparable<T>
     {
         public static int DEFAULT_CAPACITY = 100;
         public int size;   // Number of elements in heap
@@ -15,7 +13,8 @@ namespace AD
         //----------------------------------------------------------------------
         public PriorityQueue()
         {
-            throw new System.NotImplementedException();
+            array = new T[DEFAULT_CAPACITY];
+            size = 0;
         }
 
         //----------------------------------------------------------------------
@@ -23,23 +22,89 @@ namespace AD
         //----------------------------------------------------------------------
         public int Size()
         {
-            throw new System.NotImplementedException();
+            return size;
         }
 
         public void Clear()
         {
-            throw new System.NotImplementedException();
+            size = 0;
         }
 
         public void Add(T x)
         {
-            throw new System.NotImplementedException();
+            if (size == array.Length)
+            {
+                T[] newArray = new T[array.Length * 2];
+                Array.Copy(array, 0, newArray, 0, array.Length);
+                array = newArray;
+            }
+            array[size] = x;
+            size++;
+            BubbleUp();
+        }
+        
+        private void BubbleUp()
+        {
+            int index = size - 1;
+            while (index > 0)
+            {
+                int parent = (index - 1) / 2;
+                if (array[index].CompareTo(array[parent]) < 0)
+                {
+                    T temp = array[index];
+                    array[index] = array[parent];
+                    array[parent] = temp;
+                }
+                else
+                {
+                    break;
+                }
+                index = parent;
+            }
         }
 
         // Removes the smallest item in the priority queue
         public T Remove()
         {
-            throw new System.NotImplementedException();
+            if (size == 0)
+            {
+                throw new Exception("Priority queue is empty");
+            }
+            T result = array[0];
+            array[0] = array[size - 1];
+            size--;
+            BubbleDown();
+            return result;
+        }
+
+        private void BubbleDown()
+        {
+            int index = 0;
+            while (index < size)
+            {
+                int left = 2 * index + 1;
+                int right = 2 * index + 2;
+                if (left >= size)
+                {
+                    break;
+                }
+                int min = left;
+                if (right < size && array[right].CompareTo(array[left]) < 0)
+                {
+                    min = right;
+                }
+                if (array[index].CompareTo(array[min]) > 0)
+                {
+                    T temp = array[index];
+                    array[index] = array[min];
+                    array[min] = temp;
+                }
+                else
+                {
+                    break;
+                }
+                index = min;
+            }
         }
 
 
@@ -49,13 +114,24 @@ namespace AD
 
         public void AddFreely(T x)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         public void BuildHeap()
         {
-            throw new System.NotImplementedException();
+            
         }
 
+        public override string ToString()
+        {
+            string output = "";
+            
+            for (int i = 0; i < size; i++)
+            {
+                output += array[i] + (i == size - 1 ? "" : " ");
+            }
+
+            return output;
+        }
     }
 }
